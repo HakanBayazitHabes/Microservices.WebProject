ea6a6577b0c08e62ae9ce939dd3d9bdef6ae4d08
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-26 19:01:48 +0300

    Signup Endpoint added

--- IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace FreeCourse.IdentityServer
 {
@@ -31,7 +32,7 @@ namespace FreeCourse.IdentityServer
             services.AddControllersWithViews();
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
