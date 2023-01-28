b407c1ef3a7cd35c21401248f47eb55879f2d7d4
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-28 12:25:54 +0300

    Basket.API(Microservices) Appsettings.json > Host(localhost) ve Port(6379) added

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,7 +1,9 @@
+using FreeCourse.Services.Basket.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
