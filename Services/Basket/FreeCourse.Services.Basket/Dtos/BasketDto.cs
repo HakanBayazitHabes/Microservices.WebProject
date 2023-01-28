a98e210f2b8ee67aa4ade695be20341d92f67288
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-28 12:16:18 +0300

    Basket.API(Microsevices) BasketDto.cs/BasketItemDto.cs added

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> basketItems { get; set; }
+
+        public decimal TotalPrice
+        {
+            get => basketItems.Sum(x => x.Price * x.Quantity);
+
+        }
+    }
+}
