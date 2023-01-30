9437ef7baad36275364fb04006ef90b4a969e358
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-30 16:31:00 +0300

    Order.API(Microservices) DTOs added

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
