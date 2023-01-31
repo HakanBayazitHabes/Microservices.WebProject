8e106edd89972c92fee11893c0133e31fee732de
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-31 15:43:29 +0300

    Order.API(Microservices) some property changed

--- Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -13,6 +13,6 @@ namespace FreeCourse.Services.Order.Application.Commands
     {
         public string BuyerId { get; set; }
         public List<OrderItemDto> OrderItems { get; set; }
-        public AddressDto AddressDto { get; set; }
+        public AddressDto Address { get; set; }
     }
 }
