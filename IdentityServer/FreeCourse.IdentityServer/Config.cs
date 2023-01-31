3d54478eb56d20b493ebf6a3d0f694ddc11242d6
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-01-31 17:29:37 +0300

    FakePayment.API(Microservices) Authentication added

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -20,6 +20,7 @@ namespace FreeCourse.IdentityServer
                 new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
                 new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
                 new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+                new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
                 new ApiResource(IdentityServer4.IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -41,6 +42,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("basket_fullpermission","Basket API için full erişim"),
                 new ApiScope("discount_fullpermission","Discount API için full erişim"),
                 new ApiScope("order_fullpermission","Order API için full erişim"),
+                new ApiScope("payment_fullpermission","Payment API için full erişim"),
                 new ApiScope(IdentityServer4.IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -62,7 +64,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess = true,
                     ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = { "basket_fullpermission", "discount_fullpermission", "order_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServer4.IdentityServerConstants.LocalApi.ScopeName, "roles" },
+                    AllowedScopes = { "basket_fullpermission", "discount_fullpermission", "payment_fullpermission", "order_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServer4.IdentityServerConstants.LocalApi.ScopeName, "roles" },
                     AccessTokenLifetime = 1*60*60,
                     RefreshTokenExpiration = TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
--- Services/FakePayment/FreeCourse.Services.FakePayment/Program.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Program.cs
@@ -1,8 +1,30 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Burada gelen token'ýnda userýn authenticate olmasý gerekmektedir. Bunu filter içerisine atacaðýz. AddContollers içerisinde 
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+//Token içerisinde gelen sub key'ini overload yapmaz
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "resource_payment";
+    options.RequireHttpsMetadata = false;
+});
 
-builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+
+
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
