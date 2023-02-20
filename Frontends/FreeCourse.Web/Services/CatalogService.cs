b3178220ab110e234d939e704d9b32e47531404b
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-02-20 13:11:17 +0300

    .netCore MVC CatalogService added

---
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Web.Models.Catalogs;
+using FreeCourse.Web.Services.Interface;
+
+namespace FreeCourse.Web.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCourseAsync(string courserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CategoryViewModel>> GetAllCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseViewModel> GetByCourseIdAsync(string courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interface/ICatalogService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Web.Models.Catalogs;
+
+namespace FreeCourse.Web.Services.Interface
+{
+    public interface ICatalogService
+    {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<List<CategoryViewModel>> GetAllCategoriesAsync();
+        Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
+        Task<CourseViewModel> GetByCourseIdAsync(string courseId);
+        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
+        Task<bool> DeleteCourseAsync(string courserId);
+    }
+}
