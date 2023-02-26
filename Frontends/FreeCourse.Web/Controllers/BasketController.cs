be567ee901941951bfef3c18ed2aa331d36b0f3e
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-02-27 00:43:48 +0300

    .netCore MVC BasketController ApplyDiscount/CancelDiscount Action Method added

--- Frontends/FreeCourse.Web/Controllers/BasketController.cs
+++ Frontends/FreeCourse.Web/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Web.Models.Baskets;
+using FreeCourse.Web.Models.Discounts;
 using FreeCourse.Web.Services.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -44,5 +45,23 @@ namespace FreeCourse.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput)
+        {
+            var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
+
+            TempData.Add("discountStatus", discountStatus);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> CancelApplyDiscount()
+        {
+            await _basketService.CancelApplyDiscount();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
