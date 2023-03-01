a8cfcb6e4ad17d8801842d995a59eaaf7729983e
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-03-01 23:09:22 +0300

    .netCore MVC Order CheckoutInfoInput.cs added

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name = "İl")]
+        public string Province { get; set; }
+
+        [Display(Name = "İlçe")]
+        public string District { get; set; }
+
+        [Display(Name = "Cadde")]
+        public string Street { get; set; }
+
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Adres")]
+        public string Line { get; set; }
+
+
+        [Display(Name = "Kart İsmi")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Kart Numarası")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Son Kullanama Tarihi(ay/yıl)")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV/CVC2")]
+        public string CVV { get; set; }
+    }
+}
