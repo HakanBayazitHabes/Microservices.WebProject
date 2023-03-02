b650d3ba65020ffe527a4062c2e6a097ab949830
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-03-02 23:20:17 +0300

    .netCore MVC FakePayment Microservices PAymentDto added

--- Services/FakePayment/FreeCourse.Services.FakePayment/Models/PaymentDto.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Models/PaymentDto.cs
@@ -7,5 +7,8 @@
         public string Expiration { get; set; }
         public string CVV { get; set; }
         public decimal TotalPrice { get; set; }
+
+        public OrderDto Order { get; set; }
+
     }
 }
